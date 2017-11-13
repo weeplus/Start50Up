118239fe7f3e915d0ce3e28377239252c8c89c70
weeplus <weeplus@gmail.com>
2017-11-14 01:14:10 +0700

    firebase  snapshotChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,14 @@ interface Post {
 })
 export class AppComponent implements OnInit {
   postCol: AngularFirestoreCollection<Post>;
-  posts: Observable<Post[]>;
+  //posts: Observable<Post[]>;
+  posts: any;
 
   title: string;
   content: string;
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
 
   constructor(private afs: AngularFirestore) {
 
@@ -26,11 +30,24 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.postCol = this.afs.collection('posts');
-    this.posts = this.postCol.valueChanges();
+    //this.posts = this.postCol.valueChanges();
+    this.posts = this.postCol.snapshotChanges()
+        .map(actions => {
+          return actions.map(a => {
+            const data = a.payload.doc.id;
+            const id = a.payload.doc.id;
+            return { id, data};
+          });
+        });
   }
 
   addPost() {
     //this.afs.collection('posts').add({'title': this.title, 'content': this.content});
     this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
   }
 }
